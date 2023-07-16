c8a7e069ea179684d38a7be1df4f9efda12af35e
Puranjoy Patra <>
2023-07-16 20:06:03 +0530

    Resume builder assignment is implemented

--- src/components/Border.js
+++
@@ -1,5 +0,0 @@
-const Border = function ({color, children}) {
-  return <div style={{ borderColor:color,borderStyle:"solid",borderWidth:"3px", display:"inline-block" }}>{children}</div>;
-};
-
-export default Border;
